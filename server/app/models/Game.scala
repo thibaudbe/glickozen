e4b6cd94f12981760f1da1315b9e43c1243f7323
Sam Rottenberg <sro@zengularity.com>
2016-02-26 15:34:12 +0100

    add sports list

--- server/app/models/Game.scala
+++ server/app/models/Game.scala
@@ -12,7 +12,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 
-case class Game(uuid: UUID, player: String, opponent: String, playerScore: Double, date: DateTime, confirmed: Boolean)
+case object Sports {
+  val list = Seq("tabletennis", "babyfoot", "chess", "go")
+}
+
+case class Game(uuid: UUID, sport: String, player: String, opponent: String, playerScore: Double, date: DateTime, confirmed: Boolean)
 object Game {
   implicit val gameFormat: OFormat[Game] = Json.format[Game].asInstanceOf[OFormat[Game]]
 
